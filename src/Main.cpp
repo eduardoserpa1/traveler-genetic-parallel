c8ce13153f23ebb743fa6eaaa93009bf28148a85
José Eduardo Rodrigues Serpa <dudu.rserpa@gmail.com>
2024-04-04 21:46:54 -0300

    feat: add era and make childs

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <vector>
 #include <algorithm>
+#include <omp.h>
 #include "City.h"
 #include "Route.h"
 
@@ -11,20 +12,28 @@ using namespace std;
 //GLOBAL
 vector<City> cityData;
 const string filename = "../database/estados-30.csv";
+int ERA_LIMIT = 10;
 int FIRST_POPULATION_LENGTH = 1024;
 int POPULATION_DIVISOR = 8;
 vector<Route> firstPopulation;
+vector<Route> firstPopulation2;
+vector<Route> elite;
 
 //DECLARATIONS
 void setup();
 void loadCityData();
+Route createRandomRoute();
 void createFirstElite();
+void sortVectorByDistance(vector<Route> *v);
 void generateFirstPopulation();
-void sortVectorByDistance();
+void train();
+void generateChilds(vector<Route> elite, vector<Route> *childs);
 
 //IMPLEMENTATION
 void setup(){
     loadCityData();
+    generateFirstPopulation();
+    sortVectorByDistance(&firstPopulation);
     createFirstElite();
 }
 
@@ -60,7 +69,6 @@ void loadCityData(){
     }
 }
 
-
 Route createRandomRoute(){
     vector<int> randomVector;
     vector<City> finalVector;
@@ -80,34 +88,102 @@ Route createRandomRoute(){
     return Route(finalVector);
 }
 
-
 void createFirstElite(){
     int quarter = FIRST_POPULATION_LENGTH / POPULATION_DIVISOR;
+    for (int i = 0; i < quarter; ++i) {
+        elite.push_back(firstPopulation[i]);
+    }
 }
 
-void sortVectorByDistance(vector<Route> v){
-    sort(v.begin(), v.end(), [](const auto& lhs, const auto& rhs){
-        return lhs.distance() < rhs.distance();
-    });
+void sortVectorByDistance(vector<Route> *v){
+    sort( v->begin(), v->end(), [](Route &a, Route &b){ return (a.getDistance() < b.getDistance());});
 }
 
 void generateFirstPopulation(){
     for (int i = 0; i < FIRST_POPULATION_LENGTH; ++i) {
         firstPopulation.push_back(createRandomRoute());
+        firstPopulation2.push_back(createRandomRoute());
     }
+}
+
+void train(){
+    bool running = true;
+    int currentEra = 0;
+    vector<Route> currentPopulation;
+    vector<Route> currentElite;
+    int divisor = FIRST_POPULATION_LENGTH - (FIRST_POPULATION_LENGTH / (POPULATION_DIVISOR / 2));
+
+    for (Route r:elite) {
+        currentElite.push_back(r);
+    }
+
+    for (int i = 0; i < divisor; i++) {
+        currentPopulation.push_back(firstPopulation[i]);
+    }
+
+
+    while(running) {
+        vector<Route> childs;
+
+        generateChilds(currentElite,&childs);
+
+        for (int i = 0; i < currentElite.size(); ++i) {
+            currentPopulation.push_back(currentElite[i]);
+            currentPopulation.push_back(childs[i]);
+        }
+
+        sortVectorByDistance(&currentPopulation);
+
+        currentElite.clear();
+
+        for (int i = 0; i < currentElite.size(); ++i) {
+            currentElite.push_back(currentPopulation[i]);
+        }
+
+        currentPopulation.clear();
+
+        for (int i = 0; i < divisor; ++i) {
+            currentPopulation.push_back(createRandomRoute());
+        }
+
+        cout << "Melhor rota da era " << currentEra << currentElite[0].getDistance() << endl;
+
+        if(currentEra >= ERA_LIMIT)
+            running = false;
+
+        currentEra++;
+    }
+
+    elite.clear();
+    for(Route r: currentElite){
+        elite.push_back(r);
+    }
+}
+
+void generateChilds(vector<Route> elite, vector<Route> *childs){
+    for (int i = 0; i < elite.size() / 2; ++i) {
+        vector<City> fatherDNA = elite[i].getCities();
+        vector<City> motherDNA = elite[i+1].getCities();
+
+        vector<City> child1;
+        vector<City> child2;
+
+        int half = motherDNA.size() / 2;
+
+        for (int j = 0; j < half; ++j) {
+            child1.push_back(motherDNA[j]);
+            child1.push_back(fatherDNA[j + half]);
+            child2.push_back(fatherDNA[j]);
+            child2.push_back(motherDNA[j + half]);
+        }
+    }
+
 }
 
 int main() {
     setup();
-    Route route(cityData);
 
-    route.print();
-
-    Route randomRoute = createRandomRoute();
-    Route randomRoute2 = createRandomRoute();
-
-    randomRoute.print();
-    randomRoute2.print();
+    train();
 
     return 0;
 };
